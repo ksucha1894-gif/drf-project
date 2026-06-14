280dd725647476d41050f3b01b0165e537211e6f
Ксения Рип <ksucha.18.94@gmail.comProfile image>
2026-06-14 21:41:23 +0300

    init commit 1.1

--- lms/serializers.py
+++ lms/serializers.py
@@ -1,4 +1,4 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, SerializerMethodField
 
 from .models import Course, Lesson
 
@@ -13,3 +13,17 @@ class LessonSerializer(ModelSerializer):
     class Meta:
         model = Lesson
         fields = "__all__"
+
+
+class CourseDetailSerializer(ModelSerializer):
+    lessons_count = SerializerMethodField()
+    lessons = LessonSerializer(many=True, read_only=True)
+
+    def get_lessons_count(self, obj):
+        if obj.lessons.count():
+            return obj.lessons.count()
+        return 0
+
+    class Meta:
+        model = Course
+        fields = ("name", "image", "description", "lessons_count", "lessons")
